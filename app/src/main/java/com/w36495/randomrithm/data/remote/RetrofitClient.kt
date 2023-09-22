08276838185b39c72ae0bca68cd9c48213969dc8
w36495 <w36495@naver.com>
2023-09-23 01:19:55 +0900

    [feat/RANDOM-2] Retrofit 구현

---
+++ app/src/main/java/com/w36495/randomrithm/data/remote/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.w36495.randomrithm.data.remote
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.w36495.randomrithm.data.remote.endpoints.LevelAPI
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitClient {
+
+    private val BASE_URL: String = "https://solved.ac/api/v3/"
+
+    private val moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+    val levelApi: LevelAPI by lazy {
+        retrofit.create(LevelAPI::class.java)
+    }
+
+}
