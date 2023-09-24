0208a4c1539cf09b60359e74046faa01148c16fd
w36495 <w36495@naver.com>
2023-09-25 01:50:45 +0900

    [feat/RANDOM-7] 해당 카테고리에 대한 문제 리스트를 위한 API 작성

--- app/src/main/java/com/w36495/randomrithm/data/remote/endpoints/AlgorithmAPI.kt
+++ app/src/main/java/com/w36495/randomrithm/data/remote/endpoints/AlgorithmAPI.kt
@@ -1,10 +1,15 @@
 package com.w36495.randomrithm.data.remote.endpoints
 
 import com.w36495.randomrithm.data.entity.Algorithm
+import com.w36495.randomrithm.data.entity.Problem
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AlgorithmAPI {
     @GET("tag/list")
     fun getCountOfAlgorithm(): Call<Algorithm>
+
+    @GET("search/problem")
+    fun getProblemList(@Query("query") tag: String): Call<Problem>
 }
