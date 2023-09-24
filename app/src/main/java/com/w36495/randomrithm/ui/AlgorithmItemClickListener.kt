3be96dc44ebb90be786291514c34ae6222ff49bb
w36495 <w36495@naver.com>
2023-09-25 01:51:57 +0900

    [feat/RANDOM-7] 알고리즘 카테고리 클릭이벤트 리스너 작성

---
+++ app/src/main/java/com/w36495/randomrithm/ui/AlgorithmItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.w36495.randomrithm.ui
+
+interface AlgorithmItemClickListener {
+
+    fun onClickAlgorithmItem(tag: String)
+
+}
--- app/src/main/java/com/w36495/randomrithm/ui/algorithm/AlgorithmAdapter.kt
+++ app/src/main/java/com/w36495/randomrithm/ui/algorithm/AlgorithmAdapter.kt
@@ -5,8 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.w36495.randomrithm.data.entity.AlgorithmItem
 import com.w36495.randomrithm.databinding.ItemAlgorhtimBinding
+import com.w36495.randomrithm.ui.AlgorithmItemClickListener
 
 class AlgorithmAdapter : RecyclerView.Adapter<AlgorithmViewHolder>() {
+    private lateinit var algorithmItemClickListener: AlgorithmItemClickListener
 
     private var categoryList: List<AlgorithmItem> = arrayListOf()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlgorithmViewHolder {
@@ -17,18 +19,34 @@ class AlgorithmAdapter : RecyclerView.Adapter<AlgorithmViewHolder>() {
 
     override fun onBindViewHolder(holder: AlgorithmViewHolder, position: Int) {
         holder.bind(categoryList[position])
+
+        holder.onClickItem = {
+            algorithmItemClickListener.onClickAlgorithmItem(categoryList[position].key)
+        }
     }
 
     fun setList(list: List<AlgorithmItem>) {
         categoryList = list
         notifyDataSetChanged()
     }
+
+    fun setAlgorithmItemClickListener(listener: AlgorithmItemClickListener) {
+        algorithmItemClickListener = listener
+    }
 }
 
 class AlgorithmViewHolder(
     private val binding: ItemAlgorhtimBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+
+    var onClickItem: ((RecyclerView.ViewHolder) -> Unit)? = null
     fun bind(algorithm: AlgorithmItem) {
         binding.tvCategory.text = "${algorithm.displayNames[0].name} (${algorithm.problemCount})"
+
+        itemView.setOnClickListener {
+            onClickItem?.let { onClickItem ->
+                onClickItem(this@AlgorithmViewHolder)
+            }
+        }
     }
 }
