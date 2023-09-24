c300cf8a4eb4f2e76046727c0bf515ce8dca50b8
w36495 <w36495@naver.com>
2023-09-25 01:53:05 +0900

    [feat/RANDOM-7] 알고리즘 카테고리에 대한 문제 가져오기 구현

--- app/src/main/java/com/w36495/randomrithm/ui/algorithm/AlgorithmFragment.kt
+++ app/src/main/java/com/w36495/randomrithm/ui/algorithm/AlgorithmFragment.kt
@@ -8,22 +8,32 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.w36495.randomrithm.R
 import com.w36495.randomrithm.data.entity.Algorithm
 import com.w36495.randomrithm.data.entity.AlgorithmItem
+import com.w36495.randomrithm.data.entity.Problem
+import com.w36495.randomrithm.data.entity.ProblemItem
 import com.w36495.randomrithm.data.remote.RetrofitClient
 import com.w36495.randomrithm.databinding.FragmentAlgorithmBinding
+import com.w36495.randomrithm.ui.AlgorithmItemClickListener
+import com.w36495.randomrithm.ui.MainActivity
+import com.w36495.randomrithm.ui.ProblemClickListener
+import com.w36495.randomrithm.ui.problem.ProblemFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class AlgorithmFragment : Fragment() {
+class AlgorithmFragment : Fragment(), AlgorithmItemClickListener, ProblemClickListener {
 
     private var _binding: FragmentAlgorithmBinding? = null
     private val binding: FragmentAlgorithmBinding get() = _binding!!
 
     private lateinit var algorithmAdapter: AlgorithmAdapter
 
+    private var currentTag: String? = null
+
     private val categoryList = MutableLiveData<List<AlgorithmItem>>()
+    private val problemList = MutableLiveData<List<ProblemItem>>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,9 +48,31 @@ class AlgorithmFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupRecyclerView()
-        getCategoryOfAlgorithm()
+        getCategoryList()
+
         categoryList.observe(requireActivity()) {
             algorithmAdapter.setList(it)
+        }
+
+        problemList.observe(requireActivity()) {
+            val randomProblemId = it.random().problemId
+            val bundle = Bundle().apply {
+                currentTag?.let {
+                    this.putString("tag", it)
+                }
+                this.putInt("problemId", randomProblemId)
+            }
+
+            val problemFragment = ProblemFragment().apply {
+                setProblemCLickListener(this@AlgorithmFragment)
+                arguments = bundle
+            }
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.container_fragment, problemFragment)
+                .addToBackStack(MainActivity.TAG_PROBLEM_FRAGMENT)
+                .setReorderingAllowed(true)
+                .commit()
         }
     }
 
@@ -50,20 +82,42 @@ class AlgorithmFragment : Fragment() {
             adapter = algorithmAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
+        algorithmAdapter.setAlgorithmItemClickListener(this)
     }
 
-    private fun getCategoryOfAlgorithm() {
-        RetrofitClient.algorithmAPI.getCountOfAlgorithm().enqueue(object: Callback<Algorithm> {
+    private fun getCategoryList() {
+        RetrofitClient.algorithmAPI.getCountOfAlgorithm().enqueue(object : Callback<Algorithm> {
             override fun onResponse(call: Call<Algorithm>, response: Response<Algorithm>) {
                 if (response.isSuccessful) {
                     response.body()?.let {
                         categoryList.value = it.items
                     }
+                } else {
+                    Log.d("Not-Success(getCategoryList)", response.code().toString())
                 }
             }
 
             override fun onFailure(call: Call<Algorithm>, t: Throwable) {
-                Log.d("ALGORITHM_LIST(getCategoryOfAlgorithm)", t.localizedMessage)
+                Log.d("Failed(getCategoryList)", t.localizedMessage)
+            }
+        })
+    }
+
+    private fun getProblemList(tag: String) {
+        val requestQuery = "solvable:true+tag:${tag}"
+
+        RetrofitClient.algorithmAPI.getProblemList(requestQuery).enqueue(object : Callback<Problem> {
+            override fun onResponse(call: Call<Problem>, response: Response<Problem>) {
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        problemList.value = it.items
+                    }
+                } else {
+                    Log.d("Not-Success(getProblemList)", response.code().toString())
+                }
+            }
+            override fun onFailure(call: Call<Problem>, t: Throwable) {
+                Log.d("Failed(getProblemList)", t.localizedMessage)
             }
         })
     }
@@ -71,5 +125,14 @@ class AlgorithmFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onClickAlgorithmItem(tag: String) {
+        currentTag = tag
+        getProblemList(tag)
+    }
+
+    override fun onClickNextProblem(tag: String) {
+        getProblemList(tag)
     }
 }
