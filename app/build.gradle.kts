1cd46d91f05155483cdb08fd1c6a56b5b5df014f
w36495 <w36495@naver.com>
2023-12-25 17:35:23 +0900

    depend : 코루틴 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,10 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
+    // Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
