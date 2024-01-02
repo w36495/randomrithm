e9172717d1ab869870bed85a389fed3947c3e3ec
w36495 <w36495@naver.com>
2024-01-02 19:10:52 +0900

    feat : okhttp3 logging interceptor 적용

--- app/src/main/java/com/w36495/randomrithm/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/w36495/randomrithm/data/remote/RetrofitClient.kt
@@ -5,12 +5,20 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.w36495.randomrithm.data.remote.endpoints.TagAPI
 import com.w36495.randomrithm.data.remote.endpoints.LevelAPI
 import com.w36495.randomrithm.data.remote.endpoints.ProblemAPI
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
 
     private val BASE_URL: String = "https://solved.ac/api/v3/"
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+        )
+        .build()
 
     private val moshi = Moshi.Builder()
         .addLast(KotlinJsonAdapterFactory())
@@ -19,6 +27,7 @@ object RetrofitClient {
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
+        .client(httpClient)
         .build()
 
     val levelApi: LevelAPI by lazy {
