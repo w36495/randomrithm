39ac4862fd65654c2ec1fa651ba6aeab105ed574
w36495 <w36495@naver.com>
2024-02-15 13:54:11 +0900

    refactor : Network(Retrofit) 를 위한 의존성 모듈 생성 및 기존 Retrofit 정의한 파일 삭제

---
+++ app/src/main/java/com/w36495/randomrithm/di/NetworkModule.kt
@@ -0,0 +1,71 @@
+package com.w36495.randomrithm.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.w36495.randomrithm.data.remote.endpoints.LevelAPI
+import com.w36495.randomrithm.data.remote.endpoints.ProblemAPI
+import com.w36495.randomrithm.data.remote.endpoints.TagAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL: String = "https://solved.ac/api/v3/"
+
+    @Provides
+    @Singleton
+    fun provideLoggerInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(
+        logger: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshi: Moshi
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLevelAPI(retrofit: Retrofit): LevelAPI = retrofit.create(LevelAPI::class.java)
+
+    @Provides
+    @Singleton
+    fun provideTagAPI(retrofit: Retrofit): TagAPI = retrofit.create(TagAPI::class.java)
+
+    @Provides
+    @Singleton
+    fun provideProblemService(retrofit: Retrofit): ProblemAPI = retrofit.create(ProblemAPI::class.java)
+}
