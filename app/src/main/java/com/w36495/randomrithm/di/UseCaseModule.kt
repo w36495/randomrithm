e51387ebfe269c7befdbb6fc7dc0ce682698700f
w36495 <w36495@naver.com>
2024-02-15 14:46:53 +0900

    refactor : 문제(Problem) ViewModel 에 의존성 주입할 UseCase 모듈 작성

--- app/src/main/java/com/w36495/randomrithm/di/UseCaseModule.kt
+++ app/src/main/java/com/w36495/randomrithm/di/UseCaseModule.kt
@@ -1,6 +1,9 @@
 package com.w36495.randomrithm.di
 
+import com.w36495.randomrithm.domain.repository.ProblemRepository
 import com.w36495.randomrithm.domain.repository.TagRepository
+import com.w36495.randomrithm.domain.usecase.GetProblemsByLevelUseCase
+import com.w36495.randomrithm.domain.usecase.GetProblemsByTagUseCase
 import com.w36495.randomrithm.domain.usecase.GetTagsUseCase
 import dagger.Module
 import dagger.Provides
@@ -14,4 +17,14 @@ object UseCaseModule {
     fun provideGetTagsUseCase(
         tagRepository: TagRepository
     ): GetTagsUseCase = GetTagsUseCase(tagRepository)
+
+    @Provides
+    fun provideGetProblemsByLevelUseCase(
+        problemRepository: ProblemRepository
+    ): GetProblemsByLevelUseCase = GetProblemsByLevelUseCase(problemRepository)
+
+    @Provides
+    fun provideGetProblemsByTagUseCase(
+        problemRepository: ProblemRepository
+    ): GetProblemsByTagUseCase = GetProblemsByTagUseCase(problemRepository)
 }
