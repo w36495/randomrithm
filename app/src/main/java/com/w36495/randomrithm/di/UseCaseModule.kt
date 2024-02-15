948bddb26186f44559a8781cca331aef04e35107
w36495 <w36495@naver.com>
2024-02-15 14:48:03 +0900

    refactor : ViewModelComponent 에서 SingletonComponent 로 변경

--- app/src/main/java/com/w36495/randomrithm/di/UseCaseModule.kt
+++ app/src/main/java/com/w36495/randomrithm/di/UseCaseModule.kt
@@ -9,9 +9,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 object UseCaseModule {
     @Provides
     fun provideGetTagsUseCase(
