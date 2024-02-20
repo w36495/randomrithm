fe3d015403547e09672d4d738c61ed1a11406c5b
w36495 <w36495@naver.com>
2024-02-20 15:01:35 +0900

    depend : Splash Screen 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,9 @@ dependencies {
     implementation ("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
 
+    // Splash Screen
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
