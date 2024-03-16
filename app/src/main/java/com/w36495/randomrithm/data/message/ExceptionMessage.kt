bf33fbdab6e0ab094d3bb49a9e91e6dedfc45b75
w36495 <w36495@naver.com>
2024-03-16 22:45:42 +0900

    refactor : 예외처리 메세지 수정

--- app/src/main/java/com/w36495/randomrithm/data/message/ExceptionMessage.kt
+++ app/src/main/java/com/w36495/randomrithm/data/message/ExceptionMessage.kt
@@ -1,5 +1,5 @@
 package com.w36495.randomrithm.data.message
 
-enum class ExceptionMessage(message: String) {
+enum class ExceptionMessage(val message: String) {
     NonExistProblem("문제가 존재하지 않습니다.")
 }
