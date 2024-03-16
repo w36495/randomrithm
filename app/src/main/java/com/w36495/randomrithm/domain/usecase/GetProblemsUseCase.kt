c0f0e77b7e214f60888862ad8b854af6e95f2827
w36495 <w36495@naver.com>
2024-03-16 22:46:26 +0900

    refactor : DTO -> Domain Entity 변환하는 확장함수 작성

--- app/src/main/java/com/w36495/randomrithm/domain/usecase/GetProblemsUseCase.kt
+++ app/src/main/java/com/w36495/randomrithm/domain/usecase/GetProblemsUseCase.kt
@@ -2,8 +2,8 @@ package com.w36495.randomrithm.domain.usecase
 
 import com.w36495.randomrithm.data.message.ExceptionMessage
 import com.w36495.randomrithm.domain.entity.Problem
-import com.w36495.randomrithm.domain.entity.Tag
 import com.w36495.randomrithm.domain.repository.ProblemRepository
+import com.w36495.randomrithm.utils.toProblems
 import javax.inject.Inject
 
 class GetProblemsUseCase @Inject constructor(
@@ -11,26 +11,18 @@ class GetProblemsUseCase @Inject constructor(
 ){
     suspend operator fun invoke(query: String): List<Problem> {
         val queryPrefix = "solvable:true+"
-
-        val problems = mutableListOf<Problem>()
         val result = problemRepository.fetchProblems(queryPrefix + query)
 
         if (result.isSuccessful) {
             result.body()?.let { dto ->
                 if (dto.count == 0) {
-                    throw IllegalStateException(ExceptionMessage.NonExistProblem.name)
+                    throw IllegalStateException(ExceptionMessage.NonExistProblem.message)
                 }
-                dto.items.forEach {
-                    val tags = mutableListOf<Tag>()
-                    it.tags.forEach { tag ->
-                        tags.add(Tag(tag.bojTagId, tag.key, tag.displayNames[0].name, tag.problemCount))
-                    }
 
-                    problems.add(Problem(it.problemId, it.level.toString(), it.titleKo, tags.toList()))
-                }
+                return dto.toProblems()
             }
         }
 
-        return problems.toList()
+        return emptyList()
     }
 }
